247cd67bade5fc8d3460a40f16c9717047ce9749
Chizi Victor <chiziwokoma@gmail.com>
2026-04-09 18:16:03 +0100

    feat: import fonts and logo assets

--- app/components/ui/text.tsx
+++ app/components/ui/text.tsx
@@ -16,43 +16,16 @@ const headingVariants = cva("scroll-m-20 tracking-tight font-medium", {
 });
 
 export interface HeadingProps
-  extends React.HTMLAttributes<HTMLParagraphElement>,
-    VariantProps<typeof headingVariants> {}
+  extends React.HTMLAttributes<HTMLParagraphElement>, VariantProps<typeof headingVariants> {}
 
-const Heading = React.forwardRef<HTMLParagraphElement, HeadingProps>(
-  ({ className, variant = "h1", ...props }, ref) => {
-    const Comp = variant!;
+function Heading({ className, variant, ...props }: HeadingProps) {
+  const Comp = variant ?? "h1";
 
-    return <Comp className={cn(headingVariants({ variant, className }))} {...props} ref={ref} />;
-  }
-);
+  return <Comp className={cn(headingVariants({ variant, className }))} {...props} />;
+}
 
-Heading.displayName = "Heading";
+function Paragraph({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) {
+  return <p className={cn("", className)} {...props} />;
+}
 
-const Paragraph = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => {
-  return <p className={cn("", className)} {...props} ref={ref} />;
-});
-
-Paragraph.displayName = "Paragraph";
-
-const Kbd = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLElement>>(
-  ({ className, ...props }, ref) => {
-    return (
-      <kbd
-        className={cn(
-          "bg-muted shadow-border pointer-events-none items-center rounded border px-1.5 py-px text-xs shadow-[0_2px_0_1px_var(--border)]",
-          className
-        )}
-        {...props}
-        ref={ref}
-      />
-    );
-  }
-);
-
-Kbd.displayName = "Kbd";
-
-export { Kbd, Heading, Paragraph };
+export { Heading, Paragraph };
